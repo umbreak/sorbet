dedc8f01e70159c5bbe71a8948e4acb8c4c63aa5
Trevor Elliott <awesomelyawesome@gmail.com>
2022-01-24 15:56:42 -0800

    Make positional assertions trailing comments

--- test/testdata/resolver/nilable_untyped.rb
+++ test/testdata/resolver/nilable_untyped.rb
@@ -4,8 +4,7 @@
 # disable-fast-path: true
 
 
-Bad = T.type_alias {T.nilable(T.untyped)}
-                  # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+Bad = T.type_alias {T.nilable(T.untyped)} # error: `T.nilable(T.untyped)` is the same as `T.untyped`
 
 # This is a tough to handle case, as the argument to `T.nilable` in `Tricky` is
 # still `T.untyped`, but it's hard to distinguish that from a case where
@@ -19,11 +18,9 @@ class A
   extend T::Sig
   extend T::Generic
 
-  X = type_member(upper: T.nilable(T.untyped))
-                       # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+  X = type_member(upper: T.nilable(T.untyped)) # error: `T.nilable(T.untyped)` is the same as `T.untyped`
 
-  sig {params(x: T.nilable(T.untyped)).void}
-               # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+  sig {params(x: T.nilable(T.untyped)).void} # error: `T.nilable(T.untyped)` is the same as `T.untyped`
   def test1(x)
   end
 
@@ -31,9 +28,7 @@ end
 
 class B < T::Struct
 
-  prop :foo, T.nilable(T.untyped)
-           # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+  prop :foo, T.nilable(T.untyped) # error: `T.nilable(T.untyped)` is the same as `T.untyped`
 
-  const :bar, T.nilable(T.untyped), default: 10
-            # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+  const :bar, T.nilable(T.untyped), default: 10 # error: `T.nilable(T.untyped)` is the same as `T.untyped`
 end
