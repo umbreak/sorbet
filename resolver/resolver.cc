467f70bef6b31eeda6c9cd5e0db71fceff0f8165
Nathan Froyd <froydnj@gmail.com>
2022-03-12 06:22:59 -1000

    really fix undeclared class/module checking

--- resolver/resolver.cc
+++ resolver/resolver.cc
@@ -1356,7 +1356,7 @@ public:
         }
 
         const auto &precedingSymKlass = precedingSymForCurDef.asClassOrModuleRef().data(ctx);
-        if (!precedingSymKlass->flags.isUndeclared) {
+        if (!precedingSymKlass->isUndeclared()) {
             // Not a filler def, but a real def
             return defaultSymbol;
         }
