50fccbc7c21938da57ba96ad510dca851b9b7193
Adam Procter <amp269@gmail.com>
2022-03-30 17:44:01 -0700

    Add support for .rbstaleupdate files

--- main/lsp/LSPTypechecker.cc
+++ main/lsp/LSPTypechecker.cc
@@ -701,6 +701,11 @@ void LSPTypechecker::setSlowPathBlocked(bool blocked) {
     slowPathBlocked = blocked;
 }
 
+bool LSPTypechecker::isSlowPathBlocked() const {
+    absl::MutexLock lck(&slowPathBlockedMutex);
+    return slowPathBlocked;
+}
+
 LSPTypecheckerDelegate::LSPTypecheckerDelegate(TaskQueue &queue, WorkerPool &workers, LSPTypechecker &typechecker)
     : typechecker(typechecker), queue{queue}, workers(workers) {}
 
